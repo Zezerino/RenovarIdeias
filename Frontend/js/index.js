d2086e6378a476f316cffdf50422be5d24f7f001
Zé <58310094+Zezerino@users.noreply.github.com>
2021-04-09 21:05:32 +0100

    updatev1.6
    adicionado metodo post no front-end
    
    Co-Authored-By: Dumbrica <44166640+Dumbrica@users.noreply.github.com>

--- Frontend/js/index.js
+++ Frontend/js/index.js
@@ -86,6 +86,40 @@ function fillObra(){
 
 }
 
+function postObra(){
+
+	console.log("yo");
+
+	
+	console.log(document.getElementById('nomeObra').value);
+	var form = {"Nome":document.getElementById('nomeObra').value};
+
+	console.log(form);
+
+	fetch("http://localhost:3000/obras",{
+		headers:{
+			'Accept': 'application/json',
+			'Content-Type': 'application/json; charset=utf-8'
+		},       
+		method: 'POST',
+		body: form
+	}).then(
+	response=>{
+		if(response.ok){
+			return response.json();
+		}else{
+			throw new Error("something went wrong");
+		}
+	}
+	).then(result=>{
+		
+		console.log("fixe");
+
+
+	});
+
+}
+
 
 function fillOperador(){
 
