3322ddd6dee0869b6c2f5933161f706b6ef2bd69
Zé <58310094+Zezerino@users.noreply.github.com>
2021-04-09 15:50:18 +0100

    updatev1.5

--- Frontend/js/index.js
+++ Frontend/js/index.js
@@ -2,6 +2,9 @@ $( document ).ready(function() {
 
 	console.log( "ready!" );
 	fillEquip();
+	fillObra();
+	fillOperador();
+
 });
 
 
@@ -37,6 +40,84 @@ function fillEquip(){
 			option.value = result.data[i].Nome;
 			option.innerHTML = result.data[i].Nome;
 			comboEquip.appendChild(option);
+		}
+
+
+	});
+
+}
+
+
+function fillObra(){
+
+	fetch("http://localhost:3000/obras",{
+		headers:{
+			'Accept': 'application/json',
+			'Content-Type': 'application/json; charset=utf-8'
+		},       
+		method: 'GET'
+	}).then(
+	response=>{
+		if(response.ok){
+			return response.json();
+		}else{
+			throw new Error("something went wrong");
+		}
+	}
+	).then(result=>{
+		
+		console.log(result);
+		console.log((result.data).length);
+		console.log(result.data[0].idObra);
+
+		var comboObra = document.getElementById("comboObra");
+
+		for (var i= 0; i < (result.data).length; i++){
+
+			var option = document.createElement("option");
+			option.id = result.data[i].idObra;
+			option.value = result.data[i].Nome;
+			option.innerHTML = result.data[i].Nome;
+			comboObra.appendChild(option);
+		}
+
+
+	});
+
+}
+
+
+function fillOperador(){
+
+	fetch("http://localhost:3000/operadores",{
+		headers:{
+			'Accept': 'application/json',
+			'Content-Type': 'application/json; charset=utf-8'
+		},       
+		method: 'GET'
+	}).then(
+	response=>{
+		if(response.ok){
+			return response.json();
+		}else{
+			throw new Error("something went wrong");
+		}
+	}
+	).then(result=>{
+		
+		console.log(result);
+		console.log((result.data).length);
+		console.log(result.data[0].idOperador);
+
+		var comboOperador = document.getElementById("comboOperador");
+
+		for (var i= 0; i < (result.data).length; i++){
+
+			var option = document.createElement("option");
+			option.id = result.data[i].idOperador;
+			option.value = result.data[i].Nome;
+			option.innerHTML = result.data[i].Nome;
+			comboOperador.appendChild(option);
 		}
 
 
