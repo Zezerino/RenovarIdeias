b22fd8a7f00f880f90cbd45f99194bfdc52b6db2
Zé <58310094+Zezerino@users.noreply.github.com>
2021-04-10 17:46:31 +0100

    updatev1.7.1
    tentativa de adicionar  post dos movimentos. mas é preciso retornar o ID das options da combobox e não o nome

--- Frontend/js/index.js
+++ Frontend/js/index.js
@@ -9,6 +9,50 @@ $( document ).ready(function() {
 
 
 
+function postMovimento(){
+
+	console.log("yo");
+
+	console.log("Test");
+	console.log(document.getElementById('comboEquip').value);
+	console.log("Test2");
+
+	var l = document.getElementById('comboEquip').childNodes[2]
+	console.log(l);
+
+	console.log(document.getElementById('checkboxLimpar').value);
+
+
+	var form = {"idEquipamento":document.getElementById('comboEquip').value, "idObra":document.getElementById('comboObra').value, "idOperador":document.getElementById('comboOperador').value, "Tipo":document.getElementById('comboTipo').value, "EstadoNaEntregaFunciona":document.getElementById('checkboxFunciona').value, "EstadoNaEntregaLimpar":document.getElementById('checkboxLimpar').value};
+
+	console.log(form);
+
+	fetch("http://localhost:3000/movimentacoes",{
+		headers:{
+			'Accept': 'application/json',
+			'Content-Type': 'application/json; charset=utf-8'
+		},       
+		method: 'POST',
+		body: JSON.stringify(form)
+	}).then(
+	response=>{
+		if(response.ok){
+			return response.json();
+		}else{
+			throw new Error("something went wrong movimentacoes");
+		}
+	}
+	).then(result=>{
+		
+		console.log("fixe");
+
+
+	});
+
+}
+
+
+
 function fillEquip(){
 
 	fetch("http://localhost:3000/equipamentos",{
@@ -27,9 +71,9 @@ function fillEquip(){
 	}
 	).then(result=>{
 		
-		console.log(result);
-		console.log((result.data).length);
-		console.log(result.data[0].idEquipamento);
+		//console.log(result);
+		//console.log((result.data).length);
+		//console.log(result.data[0].idEquipamento);
 
 		var comboEquip = document.getElementById("comboEquip");
 
@@ -40,6 +84,7 @@ function fillEquip(){
 			option.value = result.data[i].Nome;
 			option.innerHTML = result.data[i].Nome;
 			comboEquip.appendChild(option);
+
 		}
 
 
@@ -66,9 +111,9 @@ function fillObra(){
 	}
 	).then(result=>{
 		
-		console.log(result);
-		console.log((result.data).length);
-		console.log(result.data[0].idObra);
+		//console.log(result);
+		//console.log((result.data).length);
+		//console.log(result.data[0].idObra);
 
 		var comboObra = document.getElementById("comboObra");
 
@@ -105,9 +150,9 @@ function fillOperador(){
 	}
 	).then(result=>{
 		
-		console.log(result);
-		console.log((result.data).length);
-		console.log(result.data[0].idOperador);
+		//console.log(result);
+		//console.log((result.data).length);
+		//console.log(result.data[0].idOperador);
 
 		var comboOperador = document.getElementById("comboOperador");
 
