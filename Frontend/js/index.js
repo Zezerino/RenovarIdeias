38dfa8e385b1f383a8ba00e1553c227c0b78c177
Zé <58310094+Zezerino@users.noreply.github.com>
2021-04-12 00:22:46 +0100

    updatev1.7.2
    Adicionado post dos movimentos.

--- Frontend/js/index.js
+++ Frontend/js/index.js
@@ -4,28 +4,35 @@ $( document ).ready(function() {
 	fillEquip();
 	fillObra();
 	fillOperador();
+	fillEquipS();
+	fillObraS();
+	fillOperadorS();
+
 
 });
 
 
 
-function postMovimento(){
+function postEntrada(){
 
-	console.log("yo");
-
-	console.log("Test");
-	console.log(document.getElementById('comboEquip').value);
-	console.log("Test2");
-
-	var l = document.getElementById('comboEquip').childNodes[2]
-	console.log(l);
-
-	console.log(document.getElementById('checkboxLimpar').value);
+	var comboE = document.getElementById("comboEquip");
+	var comboO = document.getElementById("comboObra");
+	var comboW = document.getElementById("comboOperador");
+	var checkF = 0;
+	var checkL = 0;
 
 
-	var form = {"idEquipamento":document.getElementById('comboEquip').value, "idObra":document.getElementById('comboObra').value, "idOperador":document.getElementById('comboOperador').value, "Tipo":document.getElementById('comboTipo').value, "EstadoNaEntregaFunciona":document.getElementById('checkboxFunciona').value, "EstadoNaEntregaLimpar":document.getElementById('checkboxLimpar').value};
+  	if(document.getElementById("checkboxFunciona").checked){
+  		checkF = 1;
 
-	console.log(form);
+  	}
+
+  	if(document.getElementById("checkboxLimpar").checked){
+  		checkL = 1;
+
+  	}
+
+	var form = {"idEquipamento":comboE[comboE.selectedIndex].id, "idObra":comboO[comboO.selectedIndex].id, "idOperador":comboW[comboW.selectedIndex].id, "Tipo":"Entrada", "EstadoNaEntregaFunciona":checkF, "EstadoNaEntregaLimpar":checkL};
 
 	fetch("http://localhost:3000/movimentacoes",{
 		headers:{
@@ -50,6 +57,43 @@ function postMovimento(){
 	});
 
 }
+
+
+function postSaida(){
+
+	var comboES = document.getElementById("comboEquipS");
+	var comboOS = document.getElementById("comboObraS");
+	var comboWS = document.getElementById("comboOperadorS");
+
+
+	var form = {"idEquipamento":comboES[comboES.selectedIndex].id, "idObra":comboOS[comboOS.selectedIndex].id, "idOperador":comboWS[comboWS.selectedIndex].id, "Tipo":"Saida", "EstadoNaEntregaFunciona":null, "EstadoNaEntregaLimpar":null};
+
+	fetch("http://localhost:3000/movimentacoes",{
+		headers:{
+			'Accept': 'application/json',
+			'Content-Type': 'application/json; charset=utf-8'
+		},       
+		method: 'POST',
+		body: JSON.stringify(form)
+	}).then(
+	response=>{
+		if(response.ok){
+			return response.json();
+		}else{
+			throw new Error("something went wrong movimentacoes");
+		}
+	}
+	).then(result=>{
+		
+		console.log("fixe");
+
+
+	});
+
+}
+
+
+
 
 
 
@@ -124,6 +168,7 @@ function fillObra(){
 			option.value = result.data[i].Nome;
 			option.innerHTML = result.data[i].Nome;
 			comboObra.appendChild(option);
+
 		}
 
 
@@ -135,7 +180,7 @@ function fillObra(){
 function fillOperador(){
 
 	fetch("http://localhost:3000/operadores",{
-		headers:{
+		headvers:{
 			'Accept': 'application/json',
 			'Content-Type': 'application/json; charset=utf-8'
 		},       
@@ -155,6 +200,124 @@ function fillOperador(){
 		//console.log(result.data[0].idOperador);
 
 		var comboOperador = document.getElementById("comboOperador");
+
+		for (var i= 0; i < (result.data).length; i++){
+
+			var option = document.createElement("option");
+			option.id = result.data[i].idOperador;
+			option.value = result.data[i].Nome;
+			option.innerHTML = result.data[i].Nome;
+			comboOperador.appendChild(option);
+		}
+
+
+	});
+
+}
+
+function fillEquipS(){
+
+	fetch("http://localhost:3000/equipamentos",{
+		headers:{
+			'Accept': 'application/json',
+			'Content-Type': 'application/json; charset=utf-8'
+		},       
+		method: 'GET'
+	}).then(
+	response=>{
+		if(response.ok){
+			return response.json();
+		}else{
+			throw new Error("something went wrong");
+		}
+	}
+	).then(result=>{
+		
+		//console.log(result);
+		//console.log((result.data).length);
+		//console.log(result.data[0].idEquipamento);
+
+		var comboEquip = document.getElementById("comboEquipS");
+
+		for (var i= 0; i < (result.data).length; i++){
+
+			var option = document.createElement("option");
+			option.id = result.data[i].idEquipamento;
+			option.value = result.data[i].Nome;
+			option.innerHTML = result.data[i].Nome;
+			comboEquip.appendChild(option);
+
+		}
+
+
+	});
+
+}
+
+
+function fillObraS(){
+
+	fetch("http://localhost:3000/obras",{
+		headers:{
+			'Accept': 'application/json',
+			'Content-Type': 'application/json; charset=utf-8'
+		},       
+		method: 'GET'
+	}).then(
+	response=>{
+		if(response.ok){
+			return response.json();
+		}else{
+			throw new Error("something went wrong");
+		}
+	}
+	).then(result=>{
+		
+		//console.log(result);
+		//console.log((result.data).length);
+		//console.log(result.data[0].idObra);
+
+		var comboObra = document.getElementById("comboObraS");
+
+		for (var i= 0; i < (result.data).length; i++){
+
+			var option = document.createElement("option");
+			option.id = result.data[i].idObra;
+			option.value = result.data[i].Nome;
+			option.innerHTML = result.data[i].Nome;
+			comboObra.appendChild(option);
+
+		}
+
+
+	});
+
+}
+
+
+function fillOperadorS(){
+
+	fetch("http://localhost:3000/operadores",{
+		headvers:{
+			'Accept': 'application/json',
+			'Content-Type': 'application/json; charset=utf-8'
+		},       
+		method: 'GET'
+	}).then(
+	response=>{
+		if(response.ok){
+			return response.json();
+		}else{
+			throw new Error("something went wrong");
+		}
+	}
+	).then(result=>{
+		
+		//console.log(result);
+		//console.log((result.data).length);
+		//console.log(result.data[0].idOperador);
+
+		var comboOperador = document.getElementById("comboOperadorS");
 
 		for (var i= 0; i < (result.data).length; i++){
 
