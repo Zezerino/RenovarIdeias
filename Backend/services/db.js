40f6d705a8571d9ddb030748025940a3a65eed79
Zé <58310094+Zezerino@users.noreply.github.com>
2021-04-23 18:42:04 +0100

    Update db.js
    update connection pools

--- Backend/services/db.js
+++ Backend/services/db.js
@@ -1,9 +1,24 @@
 const mysql = require('mysql2/promise');
 const config = require('../config');
 
+var connection;
+
+
+
+
+async function connect(){
+	connection = await mysql.createConnection(config.db);
+}
+
+
+connect();
+
+
+
+
 async function query(sql, params) {
-  const connection = await mysql.createConnection(config.db);
-  const [results, ] = await connection.execute(sql, params);
+
+const [results, ] = await connection.execute(sql, params);
 
   return results;
 }
